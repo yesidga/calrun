cfc2474798162cca449246f4fd3ccbc436d7ccbc
yesid garcia <88802141+yesidga@users.noreply.github.com>
2023-05-18 11:19:01 -0500

    Add files via upload
    agregando archivos desde la web

---
+++ calrun.py
@@ -0,0 +1,16 @@
+#programa para calcular tiempos y ritmo de una carrera atletica
+#basados en la distancia y objetivo
+
+distancia = float(input())
+ritmo = float(input())
+tiempo = float(input())
+n_u = ""
+categoria = input("")
+
+ 
+def calculo (a,b):
+    result = a + b
+    return result
+
+
+
